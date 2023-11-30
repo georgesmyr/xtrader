b8e4d1ca7c0c3718999373fbd250d1ebfbac8f74
georgesmyr <70004539+georgesmyr@users.noreply.github.com>
2023-11-30 23:30:25 +0100

    call_api fix

--- xtrader/utils.py
+++ xtrader/utils.py
@@ -1,6 +1,6 @@
 import requests
 
-def call_api(self, endpoint: str):
+def call_api(endpoint: str):
         response = requests.get(endpoint)
         if response.status_code != 200:
             raise ValueError(f'Invalid API response: {response}')
