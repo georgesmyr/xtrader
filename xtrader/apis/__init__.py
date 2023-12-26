5bf56d0caea4b6c8ba1ab6b861344955a40e33a2
georgesmyr <70004539+georgesmyr@users.noreply.github.com>
2023-12-26 01:56:47 +0100

    changed directory

--- xtrader/apis/__init__.py
+++
@@ -1,4 +0,0 @@
-from xtrader.apis import stocks
-from xtrader.apis import crypto
-
-__all__ = ['stocks', 'crypto']
---
+++ xtrader/apis/rest/__init__.py
@@ -0,0 +1,8 @@
+from xtrader.apis.rest import stocks
+from xtrader.apis.rest import crypto
+from xtrader.apis.rest import forex
+from xtrader.apis.rest import news
+from xtrader.apis.rest import macroeconomic
+from xtrader.apis.rest import fundamentals
+
+__all__ = ['stocks', 'crypto', 'forex', 'news', 'macroeconomic', 'fundamentals']
