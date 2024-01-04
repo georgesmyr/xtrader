f1d4d2d359a29f31e7280cacd7f499b47d327838
georgesmyr <70004539+georgesmyr@users.noreply.github.com>
2024-01-04 19:22:34 +0100

    added select_columns

--- xtrader/utils.py
+++ xtrader/utils.py
@@ -1,2 +1,15 @@
+import pandas as pd
 
 TIME_SYMBOLS = {"hour": "h", "day": "d", "month": "m"}
+
+def select_columns(df: pd.DataFrame, name: str, drop_name: bool = False):
+    """Selects columns from a dataframe"""
+    # Select columns that start with name
+    df_selected = df[[column for column in df.columns if column.startswith(name)]]
+    
+    # Drop name from column names
+    if drop_name:
+        df_selected.columns = [column[len(name) + 1:] for column in df_selected.columns]
+
+    return df_selected
+
